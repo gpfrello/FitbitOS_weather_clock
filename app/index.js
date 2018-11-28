1a24fe90af90e1f660b5b483f4ca30e41753fd51
Giampaolo Frello <gp.frello@gmail.com>
2018-11-28 12:48:35 +0900

    Initial upload

---
+++ app/index.js
@@ -0,0 +1,89 @@
+import clock from "clock";
+import document from "document";
+import { preferences } from "user-settings";
+import { units } from "user-settings";
+import * as util from "../common/utils";
+
+// Import the messaging module
+import * as messaging from "messaging";
+import document from "document";
+
+// Import clock preference (12h or 24h format)
+const clockPref = preferences.clockDisplay;
+
+// Import measure units 
+const measureUnitsPref = units.distance;
+
+// Set clock granularity (minutes or seconds)
+clock.granularity = "minutes";
+
+// Get a handle on the <text> element
+const timeHandle = document.getElementById("timeLabel");
+const temperatureHandle = document.getElementById("temperatureLabel");
+const meteoHandle = document.getElementById("meteoLabel");
+const locationHandle = document.getElementById("locationLabel");
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  let hoursSuffix;
+  if (hours > 12) {hoursSuffix = " pm"} else {hoursSuffix = " am"};
+  if (clockPref === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+    console.log(hours);
+  } else {
+    // 24h format
+    hours = util.zeroPad(hours);
+    hoursSuffix = "";
+  }
+  let mins = util.zeroPad(today.getMinutes());
+  timeHandle.text = `${hours}:${mins} ${hoursSuffix}`;
+}
+
+// Weather module
+
+// Request weather data from the companion
+function fetchWeather() {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // Send a command to the companion
+    messaging.peerSocket.send({
+      command: 'weather'
+    });
+  }
+}
+
+// Display the weather data received from the companion
+function processWeatherData(data) {
+  if (measureUnitsPref === "us ") {
+    temperatureHandle.text = ((data.temperature * 9 / 5) +32) + " °F";
+  }
+  else {
+    temperatureHandle.text = data.temperature + " °C";
+  }
+  temperatureHandle.text = data.temperature + " °C";
+  meteoHandle.text = data.meteo;
+  locationHandle.text = data.location;
+}
+
+// Listen for the onopen event
+messaging.peerSocket.onopen = function() {
+  // Fetch weather when the connection opens
+  fetchWeather();
+}
+
+// Listen for messages from the companion
+messaging.peerSocket.onmessage = function(evt) {
+  if (evt.data) {
+    processWeatherData(evt.data);
+  }
+}
+
+// Listen for the onerror event
+messaging.peerSocket.onerror = function(err) {
+  // Handle any errors
+  console.log("Connection error: " + err.code + " - " + err.message);
+}
+
+setInterval(fetchWeather, 60 * 1000 * 60);
---
+++ companion/index.js
@@ -0,0 +1,60 @@
+// Import the messaging module
+import * as messaging from "messaging";
+import { geolocation } from "geolocation";
+
+var API_KEY = "7a67a32640cd790bb6af48d05ab192d0";
+
+// Fetch the weather from OpenWeather
+function queryOpenWeather() {
+  geolocation.getCurrentPosition(locationSuccess, locationError);
+  function locationSuccess(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    console.log("latitude: " + lat);
+    console.log("langitude: " + long);
+    var linkApi = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon="  + long + "&units=metric" + "&APPID=" + API_KEY;
+  fetch(linkApi)
+  .then(function (response) {
+      response.json()
+      .then(function(data) {
+        // We just want some data
+        var weather = {
+          temperature: data.main.temp, meteo: data.weather[0].main, location: data["name"]
+        }
+        // Send the weather data to the device
+        returnWeatherData(weather);
+      });
+  })
+  .catch(function (err) {
+    console.log("Error fetching weather: " + err);
+  });
+ };
+ function locationError(error) {
+  console.log("Error: " + error.code,
+              "Message: " + error.message);
+}
+}
+
+// Send the weather data to the device
+function returnWeatherData(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // Send a command to the device
+    messaging.peerSocket.send(data);
+  } else {
+    console.log("Error: Connection is not open");
+  }
+}
+
+// Listen for messages from the device
+messaging.peerSocket.onmessage = function(evt) {
+  if (evt.data && evt.data.command == "weather") {
+    // The device requested weather data
+    queryOpenWeather();
+  }
+}
+
+// Listen for the onerror event
+messaging.peerSocket.onerror = function(err) {
+  // Handle any errors
+  console.log("Connection error: " + err.code + " - " + err.message);
+}
